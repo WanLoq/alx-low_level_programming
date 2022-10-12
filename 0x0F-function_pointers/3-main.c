f525dab1d99ee0bbbcf40b2325343baca8f71cb5
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-10-13 00:15:10 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 /**
  * main - program that perfroms simple operations
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: array of arguments.
  *
  * Return: Always 0 (Success)
  */
