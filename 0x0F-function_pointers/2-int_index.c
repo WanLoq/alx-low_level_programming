5b92fce7849bc7962fb310db2c503b66d7a3e66a
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-10-13 00:10:05 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
 /**
  * int_index - earches for an integer
  * @array: array to search in
- * @size: size of the array
+ * @size: size of the array.
  * @cmp: pointer to the comparing function
  *
  * Return: index of the first element for which
