58ffdbd2f46d003afc3e14f5c3b9fb665f1c8754
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-12-12 20:08:10 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/WanLoq/ALX-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://https://github.com/WanLoq/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
