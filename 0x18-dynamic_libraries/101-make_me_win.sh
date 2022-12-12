e8b3f478bd21b02c92eeed5217ef76db2d689eb7
WanLoq <mcjinkz@gmail.com>
2022-12-12 23:13:46 +0100

    update 6

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-wget -P /tmp https://github.com/WanLoq/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-=======
-wget -P /tmp https://https://github.com/WanLoq/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
->>>>>>> 58ffdbd2f46d003afc3e14f5c3b9fb665f1c8754
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
